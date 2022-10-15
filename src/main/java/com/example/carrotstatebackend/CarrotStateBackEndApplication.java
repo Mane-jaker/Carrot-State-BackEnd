318eab6637419c83b143384813337b9a1c774951
Jorge Brandon Chandomi Esponda <Sr.conejo.uwu@gmail.com>
2022-10-15 16:42:30 -0500

    first commit

---
+++ src/main/java/com/example/carrotstatebackend/CarrotStateBackEndApplication.java
@@ -0,0 +1,11 @@
+package com.example.carrotstatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotStateBackEndApplication {
+
+    public static void main(String[] args) {SpringApplication.run(CarrotStateBackEndApplication.class, args);}
+
+}
---
+++ src/test/java/com/example/carrotstatebackend/CarrotStateBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrotstatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotStateBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
